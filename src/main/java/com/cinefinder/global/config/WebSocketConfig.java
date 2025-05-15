3482a6b966e986ad0aa41db36e9832671c26bd07
seykim <kksy0917@gmail.com>
2025-05-15 10:47:56 +0900

    Feat : RabbitMQ 기초 세팅

--- src/main/java/com/cinefinder/global/config/WebSocketConfig.java
+++ src/main/java/com/cinefinder/global/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.cinefinder.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,14 +11,29 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+	@Value("${rabbitmq.websocket_endpoint}")
+	private String WEBSOCKET_ENDPOINT;
+
+	@Value("${rabbitmq.user}")
+	private String login;
+
+	@Value("${rabbitmq.password}")
+	private String passcode;
+
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic");
+		config.enableStompBrokerRelay("/topic", "/queue")
+			.setRelayHost("localhost")
+			.setRelayPort(61613)
+			.setSystemLogin(login)
+			.setSystemPasscode(passcode)
+			.setClientLogin(login)
+			.setClientPasscode(passcode);
 		config.setApplicationDestinationPrefixes("/app");
 	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/CineFinder-ws").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint(WEBSOCKET_ENDPOINT).setAllowedOrigins("*").withSockJS();
 	}
 }
