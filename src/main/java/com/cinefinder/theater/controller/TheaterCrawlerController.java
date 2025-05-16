5973d6e80b749a87b7566d9133c32748eb193318
김정욱 <axzsd96301@gmail.com>
2025-05-16 14:01:56 +0900

    feat: 프론트엔드에서 사용할 영화 스케쥴 호출 api 구현 (#31)
    * feat: 특정 위치 근처의 3사 영화 상영 정보 조회 기능 구현
    
    * feat: 영화별로 상영 정보를 묶어 응답하는 API 구현
    
    * refactor: 영화 관련 정보를 repository가 아닌 service에서 가져오도록 수정
    
    * fix: 영화관 크롤링 과정 중 예외 처리 추가
    
    * feat: CGV에서 DB에 조회가 안되는 영화는 넘기도록 처리
    
    * feat: 영화 시작시간 필터링 기능 추가
    
    * feat: 영화 시간 필터링 기준이 없으면 모든 시간의 영화가 나오도록 구현
    
    * fix: 영화 DB에 포스터가 없는 경우 처리 추가

--- src/main/java/com/cinefinder/theater/controller/TheaterCrawlerController.java
+++ src/main/java/com/cinefinder/theater/controller/TheaterCrawlerController.java
@@ -4,7 +4,7 @@ import com.cinefinder.global.mapper.ResponseMapper;
 import com.cinefinder.global.response.BaseResponse;
 import com.cinefinder.global.util.statuscode.ApiStatus;
 import com.cinefinder.theater.data.Theater;
-import com.cinefinder.theater.service.TheaterRoutingService;
+import com.cinefinder.theater.service.TheaterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,10 +19,10 @@ import java.util.Map;
 @RequestMapping("/api/theater")
 public class TheaterCrawlerController {
 
-    private final TheaterRoutingService theaterRoutingService;
+    private final TheaterService theaterService;
 
     @PostMapping("/sync")
     public ResponseEntity<BaseResponse<Map<String, List<Theater>>>> importTheater() {
-        return ResponseMapper.successOf(ApiStatus._OK, theaterRoutingService.getTheaterInfosAfterSync(), TheaterCrawlerController.class);
+        return ResponseMapper.successOf(ApiStatus._OK, theaterService.getTheaterInfosAfterSync(), TheaterCrawlerController.class);
     }
 }
