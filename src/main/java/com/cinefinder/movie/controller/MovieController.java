10da2656d5acc325451f4243390c4834618d600c
brobro332 <332bro@naver.com>
2025-05-19 09:48:43 +0900

    refactor: 엔드포인트 정리

--- src/main/java/com/cinefinder/movie/controller/MovieController.java
+++ src/main/java/com/cinefinder/movie/controller/MovieController.java
@@ -20,17 +20,17 @@ public class MovieController {
     private final BoxOfficeService boxOfficeService;
     private final MovieDetailService movieDetailService;
 
-    @GetMapping("/daily-box-office")
+    @GetMapping("/box-office/daily")
     public ResponseEntity<BaseResponse<List<BoxOffice>>> fetchDailyBoxOfficeInfo() {
         return ResponseMapper.successOf(ApiStatus._OK, boxOfficeService.getDailyBoxOfficeInfo(), MovieController.class);
     }
 
-    @GetMapping("/movie-details")
+    @GetMapping("/details")
     public ResponseEntity<BaseResponse<MovieDetails>> fetchMovieDetails(@RequestParam String title) {
         return ResponseMapper.successOf(ApiStatus._OK, movieDetailService.getMovieDetails(title), MovieController.class);
     }
 
-    @PostMapping("/multiplexMovieDetails")
+    @PostMapping("/details/multiplex")
     public ResponseEntity<BaseResponse<Void>> fetchMultiplexMovieDetails() {
         movieDetailService.fetchMultiplexMovieDetails();
         return ResponseMapper.successOf(ApiStatus._OK, null, MovieController.class);
