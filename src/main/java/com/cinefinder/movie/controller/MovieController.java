08b361e2ccc4f997bd70a45a118da61531aad718
KIMB0B <axzsd96301@gmail.com>
2025-05-28 11:49:44 +0900

    feat: 영화 검색 기능 구현

--- src/main/java/com/cinefinder/movie/controller/MovieController.java
+++ src/main/java/com/cinefinder/movie/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.cinefinder.movie.controller;
 import com.cinefinder.global.mapper.ResponseMapper;
 import com.cinefinder.global.response.BaseResponse;
 import com.cinefinder.global.util.statuscode.ApiStatus;
+import com.cinefinder.movie.data.Movie;
 import com.cinefinder.movie.data.model.BoxOffice;
 import com.cinefinder.movie.data.model.MovieDetails;
 import com.cinefinder.movie.service.MovieService;
@@ -32,5 +33,10 @@ public class MovieController {
     public ResponseEntity<BaseResponse<Void>> fetchMultiplexMovieDetails() {
         movieService.fetchMultiplexMovieDetails();
         return ResponseMapper.successOf(ApiStatus._OK, null, MovieController.class);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<BaseResponse<List<String>>> search(@RequestParam String keyword) {
+        return ResponseMapper.successOf(ApiStatus._OK, movieService.searchMovies(keyword), MovieController.class);
     }
 }
--- src/main/java/com/cinefinder/movie/data/repository/MovieRepository.java
+++ src/main/java/com/cinefinder/movie/data/repository/MovieRepository.java
@@ -32,4 +32,18 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query("SELECT m.id FROM Movie m")
     List<Long> findAllMovieIds();
+
+    @Query("""
+        SELECT m FROM Movie m
+        WHERE LOWER(m.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(m.genre) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(m.directors) LIKE LOWER(CONCAT('%', :keyword, '%'))
+           OR LOWER(m.actors) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        ORDER BY
+          CASE
+            WHEN LOWER(m.title) LIKE LOWER(CONCAT('%', :keyword, '%')) THEN 0
+            ELSE 1
+          END
+    """)
+    List<Movie> searchMoviesByKeyword(@Param("keyword") String keyword);
 }
